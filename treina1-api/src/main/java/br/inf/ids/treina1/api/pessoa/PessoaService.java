1b6c95821cc2abc9a02f5890937b5d6a3119fc9c
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2019-09-25 23:21:28 -0300

    validações

--- treina1-api/src/main/java/br/inf/ids/treina1/api/pessoa/PessoaService.java
+++ treina1-api/src/main/java/br/inf/ids/treina1/api/pessoa/PessoaService.java
@@ -1,11 +1,15 @@
 package br.inf.ids.treina1.api.pessoa;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
 
 import com.ordnaelmedeiros.jpafluidselect.querybuilder.QueryBuilder;
 
@@ -15,6 +19,16 @@ public class PessoaService {
 	@Inject
 	EntityManager em;
 	
+	@Inject
+	Validator validator;
+	
+	private void validar(Pessoa pessoa) {
+		Set<ConstraintViolation<Pessoa>> validate = validator.validate(pessoa);
+		if (!validate.isEmpty()) {
+			throw new ConstraintViolationException(validate);
+		}
+	}
+	
 	public List<Pessoa> todas() {
 		return new QueryBuilder(em)
 			.select(Pessoa.class)
@@ -23,6 +37,7 @@ public class PessoaService {
 	
 	@Transactional
 	public Long gravar(Pessoa pessoa) {
+		this.validar(pessoa);
 		em.persist(pessoa);
 		return pessoa.getId();
 	}
