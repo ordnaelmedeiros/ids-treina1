dc3c96ff411bd35a2a80f6df8e2c4df412b44787
ordnaelmedeiros <ordnaelmedeiros@users.noreply.github.com>
2019-09-30 09:48:45 -0300

    alteração para comunicação com o angular

--- treina1-api/src/main/java/br/inf/ids/treina1/core/exception/RuntimeExceptionMapper.java
+++ treina1-api/src/main/java/br/inf/ids/treina1/core/exception/RuntimeExceptionMapper.java
@@ -6,6 +6,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import org.jboss.resteasy.spi.DefaultOptionsMethodException;
 
 @Provider
 public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
@@ -13,10 +14,16 @@ public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException>
 	@Override
 	public Response toResponse(RuntimeException exception) {
 		
-		return Response
+		if (e instanceof DefaultOptionsMethodException) {
+			
+			return Response.ok("GET, POST, DELETE, PUT, OPTIONS, HEAD").build();
+			
+		} else {
+			return Response
 				.status(Status.BAD_REQUEST)
 				.entity(new ExceptionMessage(Arrays.asList(exception.getMessage())))
 				.build();
+		}
 		
 	}
 
